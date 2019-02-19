9897696da2267387ef00cba798763b60b8257c0b
LuchaoQi <lc.qi@outlook.com>
2019-02-18 23:18:52 -0500

    lc

--- Computational Genomics Data Analysis/hw2/exercise5.py
+++ Computational Genomics Data Analysis/hw2/exercise5.py
@@ -10,26 +10,30 @@ from statsmodels.stats.multitest import multipletests
 from sklearn.linear_model import Ridge
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--X", dest = "traindata", help = "training data ")
-parser.add_argument("--Y", dest = "trainresult", help = "training data result")
-parser.add_argument("--textX", dest ="testdata", help = "testing data")
-parser.add_argument("--testY", dest ="testresult", help = "testing data result")
-args = parser.parse_args()
+parser.add_argument("--X", action='store', dest="traindata", help="training data ")
+parser.add_argument("--Y", action='store', dest="trainresult", help="training data result")
+parser.add_argument("--textX", action='store', dest="testdata", help="testing data")
+parser.add_argument("--testY", action='store', dest="testresult", help="testing data result")
+option = parser.parse_args()
 
 
 # train model
 train_expression = pd.read_csv(args.traindata,header=0)
+# train_expression.csv
 X = np.array(train_expression.T)[1:]
-phen_train = pd.read_csv(args.trainresult,header=0)
+phen_train = pd.read_csv('phen_train.csv',header=0)
+# phen_train.csv
 Y = np.array(phen_train.iloc[:,1:]).reshape(145,)
 model = LogisticRegression()
 model.fit(X,Y)
 
-#test model
+# test model
 test_expression = pd.read_csv(args.testdata, header=0)
+# test_expression.csv
 x = np.array(test_expression.T)[1:]
 y_predic = model.predict(x).tolist()
-phen_test = pd.read_csv(args.testresult,header = 0)
+phen_test = pd.read_csv('phen_test.csv',header = 0)
+# phen_test.csv
 y = np.array(phen_test.iloc[:,1:])
 y = list(map(int, y))
 
@@ -49,5 +53,6 @@ for i in range(len(y)):
         FN += 1
 precision = TP/(TP+ FP)
 recall = TP/(TP + FN)
-
+print("Precision:", precision)
+print("Recall:", recall)
 
