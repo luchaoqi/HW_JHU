550d8cd77ad96530cb315ff45aa3477a7af16f89
LuchaoQi <lc.qi@outlook.com>
2019-02-19 18:33:52 -0500

    lc

--- data_analysis/hw2/hw2/correlation test.R
+++
@@ -1,8 +0,0 @@
-install.packages("ggpubr")
-library("ggpubr")
-library('ggplot2')
-library(magrittr)
-x = c(10.9,7.6,11.7,11.8,12.5,8.7,10.8,10.3,10.9,9.3)
-y = c(15.6,15.1,19.4,17.1,17.2,15.5,17.6,17.6,14.4,14.6)
-cor.test(x,y,method = 'pearson')
-cor(x,y)
