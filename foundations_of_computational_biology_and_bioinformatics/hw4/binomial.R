765223b0169f497a2cb8d7965c830af0937e6296
Luchao Qi <lc.qi@outloook.com>
2019-02-28 17:56:16 -0500

    lc

---
+++ foundations_of_computational_biology_and_bioinformatics/hw4/binomial.R
@@ -0,0 +1,15 @@
+
+#Code samples 1000 values from a binomial distribution,
+#computes p-values and plots a histogram of the p-values.
+#It shows that p-values under the null are uniformly distributed.
+#The code should output an image file of the histogram.
+
+set.seed(101) #pseudo random number generator seed to 101
+numbers  = rbinom(1000,500,0.5) #N = 1000, n = 500, p = 0.5
+
+pvalues = (1-pbinom(numbers, 500, 0.5)) #calculate p-values
+
+png(file='PvalueHistogram.png')
+hist(pvalues) #add freq = false to get density for y-axis
+
+dev.off()
