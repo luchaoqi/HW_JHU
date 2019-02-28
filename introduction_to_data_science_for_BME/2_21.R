f122a18de0c22e1477426dc4c5698249ddc5d22d
Luchao Qi <lc.qi@outloook.com>
2019-02-28 16:50:14 -0500

    lc

---
+++ introduction_to_data_science_for_BME/2_21.R
@@ -0,0 +1,16 @@
+library(magrittr)
+library(dplyr)
+library(MRIcloudT1volumetrics)
+read.csv()
+dat = read.table(raw.asd, header = true) ?????????????????????name
+prop.table
+table(day$year)
+library(tidyverse)
+dat = read_tsv()
+
+
+library(MRIcloudT1volumetrics)
+dat = readSubject()
+is.list(dat)
+subject2df( )
+
