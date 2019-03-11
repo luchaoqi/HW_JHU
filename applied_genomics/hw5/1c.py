43e625295a20a66af5410f4e3119351555041e08
LuchaoQi <lc.qi@outlook.com>
2019-03-10 20:40:02 -0400

    lc

--- applied_genomics/hw5/1c.py
+++ applied_genomics/hw5/1c.py
@@ -9,6 +9,7 @@ for lines in f:
 	line = lines.strip().rstrip('\n').split("\"")
 	a.append(line[1])
 dat = pd.Series(a)
+print(dat.value_counts())
 b = dat.value_counts().tolist()
 print('Max: {0}'.format(np.max(b)))
 print('Min: {0}'.format(np.min(b)))
