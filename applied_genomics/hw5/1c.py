7858a0a88a26dfc3f541ac3f1dd0fa840db7d8cc
LuchaoQi <lc.qi@outlook.com>
2019-03-11 21:16:59 -0400

    lc

--- applied_genomics/hw5/1c.py
+++ applied_genomics/hw5/1c.py
@@ -1,3 +1,4 @@
+#awk '$3 == "transcript" && /protein_coding/' Homo_sapiens.GRCh38.87.gtf | tr ';' '\t'| cut -f 9 > exon.txt
 import numpy as np
 import pandas as pd
 
@@ -9,7 +10,7 @@ for lines in f:
 	line = lines.strip().rstrip('\n').split("\"")
 	a.append(line[1])
 dat = pd.Series(a)
-print(dat.value_counts())
+#print(dat.value_counts())
 b = dat.value_counts().tolist()
 print('Max: {0}'.format(np.max(b)))
 print('Min: {0}'.format(np.min(b)))
